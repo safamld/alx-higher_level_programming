1375a436ecfcc67d526fb8a430fbccc26a640cd5
safa <safaaamd7@gmail.com>
2024-02-20 07:56:03 -0800

    fix it

--- 0x0D-SQL_introduction/1-create_database_if_missing.sql
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -1,3 +1,3 @@
 -- creates the sql server user_0d_1 and grant all priviledges
-CREATE USER IF NOT EXISTS hbtn_0c_0@localhost IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES ON *.* TO user_0d_1@localhost;
+CREATE USER IF NOT EXISTS user_0d_0@localhost IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON * . * TO user_0d_1@localhost;
