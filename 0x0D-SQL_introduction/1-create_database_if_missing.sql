48ac63f25b85aa7fcf565a73b01820b9ea9019e7
safa <safaaamd7@gmail.com>
2024-02-20 06:27:51 -0800

    script that creates the database hbtn_0c_0 in your MySQL server

---
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -0,0 +1,3 @@
+-- creates the sql server user_0d_1 and grant all priviledges
+CREATE USER IF NOT EXISTS user_0d_10localhost IDENTIFIED BY 'user_0d-1-pwwd' ;
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1@localhost;
